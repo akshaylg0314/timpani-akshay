07fdf128fb79e7b7a7ca2d84c7f2841390ac4eda
Seokhee Han <seokhee.han@lge.com>
2024-11-19 17:38:40 +0900

    manage gnuplot source data and trace data of each node
      usage: scripts/mandata/send_datas.sh node_id to_scp
      $ scripts/mandata/send_datas.sh 1 seokhee.han@10.177.247.121:/home/seokhee.han/

---
+++ scripts/mandata/send_datas.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+workdir=$PWD
+c_date=`date +%y%m%d%H%M`
+
+f_gpdata=node$1.$c_date.gpdata
+f_trace=trace.node$1.$c_date
+sendto=$2
+
+sudo cp /sys/kernel/debug/tracing/trace $workdir/$f_trace
+#sudo sh -c "echo > /sys/kernel/debug/tracing/trace"
+sudo mv $workdir/build/node$1.gpdata $workdir/$f_gpdata
+
+sudo chmod 644 $workdir/$f_gpdata $workdir/$f_trace
+scp $workdir/$f_gpdata $workdir/$f_trace $sendto
+sudo rm $workdir/$f_gpdata $workdir/$f_trace
