c04ed84fbc65972483ecd311df9f5c51a25af101
Seokhee Han <seokhee.han@lge.com>
2024-11-19 11:55:44 +0900

    remove irq event and add syscall events with entering and wxiting rt_sigtimedwait

--- src/trace.c
+++ src/trace.c
@@ -9,15 +9,17 @@ int tracer_fd = -1;
 int marker_fd = -1;
 int en_ev_sched_fd, en_ev_irq_fd;
 int en_ev_timer_fd, en_ev_signal_fd;
+int en_ev_enter_sigwait_fd, en_ev_exit_sigwait_fd;
 
 const char *tracing_path = "/sys/kernel/debug/tracing";
 
 const char *tracer_path = "/sys/kernel/debug/tracing/tracing_on";
 const char *marker_path = "/sys/kernel/debug/tracing/trace_marker";
 const char *en_ev_sched_path = "/sys/kernel/debug/tracing/events/sched/enable";
-const char *en_ev_irq_path = "/sys/kernel/debug/tracing/events/irq/enable";
 const char *en_ev_timer_path = "/sys/kernel/debug/tracing/events/timer/enable";
 const char *en_ev_signal_path = "/sys/kernel/debug/tracing/events/signal/enable";
+const char *en_ev_enter_sigwait_path = "/sys/kernel/debug/tracing/events/syscalls/sys_enter_rt_sigtimedwait/enable";
+const char *en_ev_exit_sigwait_path = "/sys/kernel/debug/tracing/events/syscalls/sys_exit_rt_sigtimedwait/enable";
 
 static int enable_event(const char *path) {
 	int fd;
@@ -41,9 +43,10 @@ static void disable_event(int fd, const char *path) {
 
 void tracer_on(void) {
 	en_ev_sched_fd = enable_event(en_ev_sched_path);
-	en_ev_irq_fd = enable_event(en_ev_irq_path);
 	en_ev_timer_fd = enable_event(en_ev_timer_path);
 	en_ev_signal_fd = enable_event(en_ev_signal_path);
+	en_ev_enter_sigwait_fd = enable_event(en_ev_enter_sigwait_path);
+	en_ev_exit_sigwait_fd = enable_event(en_ev_exit_sigwait_path);
 
 	tracer_fd = open(tracer_path, O_WRONLY);
 	if (tracer_fd >= 0) {
@@ -66,9 +69,10 @@ void tracer_off(void) {
 	}
 
 	disable_event(en_ev_sched_fd, en_ev_sched_path);
-	disable_event(en_ev_irq_fd, en_ev_irq_path);
 	disable_event(en_ev_timer_fd, en_ev_timer_path);
 	disable_event(en_ev_signal_fd, en_ev_signal_path);
+	disable_event(en_ev_enter_sigwait_fd, en_ev_enter_sigwait_path);
+	disable_event(en_ev_exit_sigwait_fd, en_ev_exit_sigwait_path);
 }
 
 void write_trace_marker(const char *fmt, ...) {
