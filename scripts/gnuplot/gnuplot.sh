b10ec63b904b9116ffb1332f2dc58976c26e81cf
Chan Jeong <chan.jeong@lge.com>
2024-11-20 10:57:16 +0900

    gnuplot: add gnuplot scripts for gantt chart by cpu
    This commit also changes the parameter format of 'gnuplot.sh':
      $ ./gnuplot.sh nodes.gpdata
    
    There will be 'nodes-task.png' for task chart and 'nodes-cpu.png'
    for cpu chart.

--- scripts/gnuplot/gnuplot.sh
+++ scripts/gnuplot/gnuplot.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
-	echo "Usage: $0 input_file output_file"
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 input_file"
 	exit 1
 fi
 
 GP_SCRIPT_DIR=$(dirname $0)
 GP_INPUT_FILE=$1
-GP_OUTPUT_FILE=$2
+GP_OUTPUT_FILE_BASE=${GP_INPUT_FILE%%.*}
+GP_OUTPUT_FILE_TASK="$GP_OUTPUT_FILE_BASE-task.png"
+GP_OUTPUT_FILE_CPU="$GP_OUTPUT_FILE_BASE-cpu.png"
 
 export GP_SCRIPT_DIR
 export GP_INPUT_FILE
 
-exec gnuplot $GP_SCRIPT_DIR/ttsched.gpi > $GP_OUTPUT_FILE
+gnuplot $GP_SCRIPT_DIR/ttsched.gpi > $GP_OUTPUT_FILE_TASK
+gnuplot $GP_SCRIPT_DIR/ttsched-cpu.gpi > $GP_OUTPUT_FILE_CPU
+
+exit 0
