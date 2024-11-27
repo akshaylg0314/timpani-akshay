2a0b4106b8c1e676d30c02b8ef002bc446072728
Chan Jeong <chan.jeong@lge.com>
2024-11-27 05:55:23 +0000

    exprocs: support for specified cpu runtime and clean-up
    This commit adds an option for specific cpu runtime:
    Usage: ./exprocs name period_in_us [runtime_in_us]
      $ ./exprocs wakeeA  10000   3000

--- test/exprocs.c
+++ test/exprocs.c
@@ -11,88 +11,79 @@
 
 #include "exprocs.h"
 
-#include "libttsched.h"
-
 char pr_name[16];
 
-clockid_t clockid = CLOCK_MONOTONIC;
-struct timespec now, before;
-
-// Function to perform calculations upon receiving SIGNO_TT
-void do_calculations(int signo) {
-	// Do some calculation here, for example:
-	int x = 5;
-	int y = 6;
-	//printf("Result of %d + %d is: %d\n", x, y, x+y);
-	x+y;
-}
+clockid_t clockid = CLOCK_REALTIME;
 
 unsigned int task_period;	// microseconds
+unsigned int task_runtime;	// microseconds
 unsigned long long jitter_cnt;
-// Function to handle SIGNO_TT signals and execute do_calculations()
-static void sig_handler(int signo, siginfo_t *info, void *context) {
-	if (signo == SIGNO_TT) {
-		before = now;
-		clock_gettime(clockid, &now);
 
+static uint64_t get_cpu_time(void)
+{
+	struct timespec ts;
+
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+	return ts_ns(ts);
+}
+
+// Function for real-time workload
+static void do_workload(void)
+{
+	static struct timespec before;
+	struct timespec now;
+	uint64_t start_ns, runtime_ns;
+
+	clock_gettime(clockid, &now);
+
+	if (ts_ns(before)) {
 		int jitter = diff(ts_ns(now), ts_ns(before)) / NSEC_PER_USEC - task_period;
 		if (jitter < -100 || jitter > 100) {
 			printf("%s: jitter(%llu) for execution: %d us\n", pr_name, ++jitter_cnt, jitter);
 		}
-		do_calculations(SIGNO_TT); // Perform calculations when receiving SIGNO_TT
 	}
+
+	if (task_runtime) {
+		// Busy loop during the assigned runtime
+		runtime_ns = task_runtime * NSEC_PER_USEC;
+		start_ns = get_cpu_time();
+		while(1) {
+			if ((get_cpu_time() - start_ns) >= runtime_ns) break;
+		}
+	}
+
+	before = now;
+	//printf("[%lu]: %s end of handler\n", ns, pr_name);
 }
 
-#if 0
-void sigusr2_handler(int signo) {
-	if (signo == SIGUSR2) {
-		printf("Received SIGUSR2 signal\n");
-	}
-}
-#endif
-
-int main(int argc, char *argv[]) {
-	struct sigaction act;
-	struct sched_attr attr;
-
+int main(int argc, char *argv[])
+{
+	sigset_t sigset;
+	int signal_received; // Variable to keep track of the received signals
 	int pid = getpid();
 
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s name period_in_us [runtime_in_us]\n", argv[0]);
+		exit(1);
+	}
+
 	task_period = atoi(argv[2]);
+	if (argc > 3) {
+		task_runtime = atoi(argv[3]);
+	}
 
 	prctl(PR_SET_NAME, (unsigned long)argv[1], 0, 0, 0);
 	prctl(PR_GET_NAME, pr_name, 0, 0, 0);
 
-	set_schedattr(pid, 80, SCHED_FIFO);
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGNO_TT);
+	sigprocmask(SIG_BLOCK, &sigset, NULL);
 
-	memset(&act, 0, sizeof(struct sigaction));
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = &sig_handler; // Set the signal handler function to handle SIGNO_TT
-
-	if (sigaction(SIGNO_TT, &act, NULL) == -1) {  // Register the signal handler for SIGNO_TT
-		perror("Failed to register sigaction");
-		return EXIT_FAILURE;
-	}
-
-#if 0
-	act.sa_sigaction = &sigusr2_handler;
-
-	if (sigaction(SIGUSR2, &act, NULL) == -1) {
-		perror("Failed to register sigaction");
-		return EXIT_FAILURE;
-	}
-#endif
-
-	printf("%s (%d) with period %d us is waiting for signal(%d)\n", pr_name, pid, task_period, SIGNO_TT);
-
-	int signal_received = 0; // Variable to keep track of the received signals
+	printf("%s (%d) with period %u ms & runtime %u ms is waiting for signal(%d)\n",
+		pr_name, pid, task_period/1000, task_runtime/1000, SIGNO_TT);
 
 	while (1) {
-		siginfo_t info;
-
-		clock_gettime(clockid, &now);
-
-		if (sigwait(&act.sa_mask, &signal_received) == -1) {   // Wait for a signal from act.sa_mask
+		if (sigwait(&sigset, &signal_received) == -1) {   // Wait for a signal from act.sa_mask
 			perror("Failed to wait for signals");
 			return EXIT_FAILURE;
 		}
@@ -101,9 +92,9 @@ int main(int argc, char *argv[]) {
 			printf("signal %d is received!!!\n", signal_received);
 			continue;  // If the received signal is not SIGNO_TT, continue waiting
 		}
-		printf("Received SIGNO_TT\n");
+
+		do_workload();
 	}
 
 	return EXIT_SUCCESS;
 }
-
